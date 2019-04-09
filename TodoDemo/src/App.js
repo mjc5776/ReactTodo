52ab5cb78b1b0c6ad4b3d2dbc50483ff99aaf290
Mike Corey <mjcorey57@gmail.com>
2019-04-09 10:56:14 -0600

    React Todo

---
+++ TodoDemo/src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import NewTodoForm from './NewTodoForm';
+import TodoList from './TodoList';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello Coding Garden!",
+      newTodo: '',
+      todos: [{
+        title: 'Learn React',
+        done: false
+      }, {
+        title: 'Learn JSX',
+        done: false
+      }]
+    };
+  }
+  newTodoChanged(event) {
+    console.log(event.target.value);
+    this.setState({
+      newTodo: event.target.value
+    }); 
+  }
+
+
+  formSubmitted(event) {
+    event.preventDefault()
+    this.setState({
+      newTodo: '',
+      todos: [...this.state.todos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    }); 
+  }
+
+  toggleTodoDone(event, index) {
+    console.log(event.target.checked);
+    const todos = [...this.state.todos]; //copy the array
+    todos[index] = {...todos[index]}; //copy the todo 
+    todos[index].done = event.target.checked; //update the done property on copied todo
+    console.log(todos);
+    
+    this.setState({
+      todos
+    });
+  }
+
+  removeTodo(index) {
+    const todos = [...this.state.todos]; //copy the array
+    todos.splice(index, 1)
+    this.setState({
+      todos
+    });
+  }
+
+  allDone() {
+    const todos = this.state.todos.map(todo => {
+      return {
+        title: todo.title,
+        done: true
+      };
+    });
+
+    this.setState({
+      todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>{this.state.message}</h3>
+      <NewTodoForm 
+        newTodo = {this.state.newTodo}
+        formSubmitted={this.formSubmitted.bind(this)}
+        newTodoChanged = {this.newTodoChanged.bind(this)} />
+        <button onClick={() => this.allDone()}>All Done</button>
+        <TodoList
+          todos={this.state.todos}
+          toggleTodoDone={this.toggleTodoDone.bind(this)}
+          removeTodo={this.removeTodo.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default App;
